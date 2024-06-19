96bf62b0f75bcd96c6386998bcbc11c916c9b7ee
G.K. <74313918+DedSecRh@users.noreply.github.com>
2024-06-19 13:15:31 +0000

    Добавили токен secret  для  отладки в replit

--- config.py
+++ config.py
@@ -4,10 +4,13 @@
 # Для передачи токена создайте файл .env со следующим содержимым
 # TOKEN=ВАШ_ТОКЕН
 
+
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# ### Включить для запуска на серевере
+# load_dotenv()
 
-TOKEN: str = os.getenv('TOKEN')
-
+# TOKEN: str = os.getenv('TOKEN')
+# ### Включить для отладки в replit
+TOKEN: str = os.environ['TOKEN']
